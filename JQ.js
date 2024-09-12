c317051279715b16dde1a115e0b21e3ba3f6c909
Shaikh Mueez MD Haroon <71594607+Moiz439@users.noreply.github.com>
2024-09-12 16:39:33 +0530

    Add files via upload

---
+++ JQ.js
@@ -0,0 +1,7 @@
+function fun1()
+{
+    //$('#img1').fadeToggle(3000); // we can use with time intervel alse like : $('img1').fadeToggle(3000);
+    $('#img1').fadeToggle();  //we can use without time intervel alse like : $('img1').fadeToggle();
+}
+
+   
